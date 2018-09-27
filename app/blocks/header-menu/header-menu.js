cc4dbd320b38fa727149ae87ec77033f78351143
Yurajun <yurajun@mail.ru>
2018-09-27 16:10:49 +0300

    end adaptive header

---
+++ app/blocks/header-menu/header-menu.js
@@ -0,0 +1,65 @@
+/* global $ */
+
+import {appResponse}from '../scripts/utils/aero.mediaquery';
+
+const $html                = $('html');
+
+const $buttonOpenCloseMenu = $('.js-button-main-menu');
+const $subMenu             = $('.js-header-menu .sub-menu');
+const $backLinkSubMenu     = $('.js-header-menu .sub-menu ul li:first-child a');
+
+const $buttonExtraMenu     = $('.js-extra-menu');
+
+$buttonOpenCloseMenu.on('click', e => {
+	const $button = $(e.currentTarget);
+	if (!$button.is('.is-open')){
+		$button.addClass('is-open');
+		$html.addClass('is-open');
+	}else {
+		$button.removeClass('is-open');
+		$html.removeClass('is-open');
+	}
+});
+
+$subMenu.on('click', e => {
+	if ($html.is('.mobile')){
+		$(e.currentTarget).addClass('is-open');
+	}else {
+		$(e.currentTarget).removeClass('is-open');
+	}
+});
+
+$backLinkSubMenu.on('click', e => {
+	e.stopPropagation();
+	$(e.currentTarget).closest('.sub-menu').removeClass('is-open');
+});
+
+$buttonExtraMenu.on('click', e => {
+	console.log($(e.currentTarget).find('ul'));
+	if ($html.is('.mobile') && $(e.currentTarget).find('ul').is(':hidden')){
+		$(e.currentTarget).find('ul').slideDown();
+	}else if ($html.is('.mobile')){
+		$(e.currentTarget).find('ul').slideUp();
+	}
+});
+
+const mqOpenCloseMenu = () => {
+	if (appResponse.MediaQuery.app.bp.name === 'xs'){
+		if (!$html.is('.mobile')){
+			$html.addClass('mobile');
+			$($buttonExtraMenu).find('ul').hide();
+		}
+	}else if ($html.is('.mobile')){
+		$html.removeClass('mobile');
+		$buttonOpenCloseMenu.trigger('click');
+		$('.js-button-main-menu.is-open').trigger('click');
+		$subMenu.trigger('click');
+		$backLinkSubMenu.trigger('click');
+	}
+};
+
+mqOpenCloseMenu();
+
+$(window).on('resize', () => {
+	mqOpenCloseMenu();
+});
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,10 @@ import $ from 'jquery';
 $(() => {
 	svg4everybody();
 });
+
+// import {appResponse}from '../blocks/scripts/utils/aero.mediaquery';
+// import '../blocks/scripts/functions/equalheaights';
+
+import '../blocks/header-info/header-info';
+import '../blocks/header-menu/header-menu';
+
